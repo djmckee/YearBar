3f4fdbafd18fd74f2ddcabb57c2b25deb5045076
Dylan McKee <dylan@djmckee.co.uk>
2015-08-02 10:06:40 +0100

    Initial Commit

---
+++ YearBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YearBar
+//
+//  Created by Dylan McKee on 02/08/2015.
+//  Copyright (c) 2015 djmcke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
