ff0d91cb78530f1978527fed539810c5647df5b0
Dylan McKee <dylan@djmckee.co.uk>
2015-08-02 10:11:15 +0100

    Added Today extension.

---
+++ YearCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  YearCalculator.swift
+//  YearBar
+//
+//  Created by Dylan McKee on 02/08/2015.
+//  Copyright (c) 2015 djmcke. All rights reserved.
+//
+
+import Foundation
+
+// A simple utility class to calculate the progress throught the current year, and the number of days left
+public class YearCalculator {
+    //
+    
+}
